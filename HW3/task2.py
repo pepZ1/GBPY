f288135a4787e5b338c1fe847dd9c3fd5aff2735
aketo <aketo13@gmail.com>
2023-07-21 19:46:51 +0400

    add task2

--- HW3/task2.py
+++ HW3/task2.py
@@ -1,10 +1,29 @@
-def remove_duplicates(input_list):
-    unique_set = set(input_list)
+def fill_backpack(items, max_weight):
+    # Сортируем список вещей по возрастанию массы
+    sorted_items = sorted(items, key=lambda x: x[1])
 
-    unique_list = list(unique_set)
+    backpack_contents = []
+    current_weight = 0
 
-    return unique_list
+    for item in sorted_items:
+        # Если добавление вещи не превышает максимальную грузоподъемность, добавляем ее в рюкзак
+        if current_weight + item[1] <= max_weight:
+            backpack_contents.append(item[0])
+            current_weight += item[1]
 
-duplicated_list = [1, 2, 2, 3, 4, 4, 4, 5]
-result_list = remove_duplicates(duplicated_list)
-print("список без дубликатов:", result_list)
+    return backpack_contents
+
+
+things = {
+    "Тент": 2,
+    "Спальник": 3,
+    "Фонарик": 1,
+    "Еда": 4,
+    "Палатка": 5,
+    "Вода": 2,
+}
+
+max_backpack_weight = 10
+result = fill_backpack(list(things.items()), max_backpack_weight)
+print("Вещи в рюкзаке:")
+print(result)
