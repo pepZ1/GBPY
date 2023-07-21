451b11a36cc39451a5a02e44c031b68e5db1d951
aketo <aketo13@gmail.com>
2023-07-21 19:45:09 +0400

    add task3

---
+++ HW3/task3.py
@@ -0,0 +1,24 @@
+import re
+from collections import Counter
+
+
+def most_common_words(text, top_n=10):
+    # Очистим текст от знаков препинания и приведем к нижнему регистру
+    cleaned_text = re.sub(r'[^\w\s]', '', text).lower()
+
+    words = cleaned_text.split()
+
+    word_count = Counter(words)
+
+    sorted_word_count = dict(sorted(word_count.items(), key=lambda item: item[1], reverse=True))
+
+    most_common_words = list(sorted_word_count.keys())[:top_n]
+
+    return most_common_words
+
+
+
+text = "всем привет я ем омлет уже сто лет вот это да ведь это не беда, а я а он ведь это звон"
+result = most_common_words(text)
+print("10 самых частых слов:")
+print(result)
