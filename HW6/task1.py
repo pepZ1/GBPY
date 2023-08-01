0056f08409c19861983f40eb7ebc91566166f5f7
aketo <aketo13@gmail.com>
2023-08-01 18:36:06 +0400

    add hw6

---
+++ HW6/task1.py
@@ -0,0 +1,19 @@
+def _is_leap(year :int) -> bool:
+    return not (year % 4 != 0 or year % 100 == 0 and year % 400 != 0)
+
+
+def valid(full_date: str) -> bool:
+    date, month, year = (int(item) for item in full_date.split('.'))
+    if year < 1 or year > 9999 or month < 1 or month > 12 or date < 1 or date > 31:
+        return False
+    if month in (4, 6, 9, 11) and date > 30:
+        return False
+    if month == 2 and _is_leap(year) and date > 29:
+        return False
+    if month == 2 and not _is_leap(year) and date > 28:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    print(valid('30.2.2001'))
---
+++ HW6/task2.py
@@ -0,0 +1,4 @@
+__all__ = ['numbers', 'text', 'date']  # в init
+__all__ = ['guess']  # угадай число
+__all__ = ['secrets', 'storage']  # угадай загадку
+__all__ = ['valid']  # проверь дату
