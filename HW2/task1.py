7341c65aa5dfca54a88a6a7d527eb9c5999a32f9
aketo <aketo13@gmail.com>
2023-08-29 19:36:24 +0400

    add hw2

---
+++ HW2/task1.py
@@ -0,0 +1,23 @@
+def get_hexadecimal_representation(number):
+    hexadecimal = ""
+
+    is_negative = False
+    if number < 0:
+        is_negative = True
+        number = abs(number)
+
+    while number > 0:
+        remainder = str(number % 16)
+        hexadecimal = remainder + hexadecimal
+        number = number // 16
+
+    if is_negative:
+        hexadecimal = "-" + hexadecimal
+
+    return hexadecimal
+
+
+num = int(input("Введите целое число: "))
+result = get_hexadecimal_representation(num)
+print("Шестнадцатеричное представление числа:", result)
+print("Шестнадцатеричное представление числа через hex: ", hex(num))
